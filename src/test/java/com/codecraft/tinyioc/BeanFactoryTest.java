d21265cb199e98f67a18679b296b3240933176c2
ZhanYuSheng <1143109382@qq.com>
2019-08-31 18:32:48 +0800

    提交测试，spring-ioc测试项目

---
+++ src/test/java/com/codecraft/tinyioc/BeanFactoryTest.java
@@ -0,0 +1,17 @@
+package com.codecraft.tinyioc;
+
+import org.junit.Test;
+
+public class BeanFactoryTest {
+
+    @Test
+    public void test(){
+        BeanFactory beanFactory = new BeanFactory();
+
+        BeanDefinition beanDefinition = new BeanDefinition(new HelloWorldService());
+        beanFactory.registerBeanDefinition("helloWorldService", beanDefinition);
+        HelloWorldService helloWorldService = (HelloWorldService)beanFactory.getBean("helloWorldService");
+        helloWorldService.sayHello();
+    }
+
+}
---
+++ src/test/java/com/codecraft/tinyioc/HelloWorldService.java
@@ -0,0 +1,9 @@
+package com.codecraft.tinyioc;
+
+public class HelloWorldService {
+
+    public void sayHello(){
+        System.out.println("Hello World :)");
+    }
+
+}
